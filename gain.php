b3c5497a913476c85fac27972c840d38ab2f00fd
desaiyang <60084078+desaiyang@users.noreply.github.com>
2020-07-16 19:38:47 +0530

    first file for trng
    code for trng website

---
+++ gain.php
@@ -0,0 +1,25 @@
+<?php
+//gain.php
+//author:g
+//gain.php is the first page in trng website. gain.php consists of 6 iframes.
+//-----------------------------------------------------------------------
+//header of the webpage
+//WIDTH(10%)	|(10%)							|(10%)
+//HEIGHT(10%)	|(10%)							|(10%)
+//logo 			|main menus						|lock
+//(new1.php)	|(new1.1.php)					|(new1.2.1.php)
+//----------|-----------------------------------|------------------------
+//MAIN CONTENT LEFT					| MAIN CONTENT RIGHT
+//WIDTH(48%) HEIGHT(85%)			| WIDTH(48%) HEIGHT(85%)
+//									|
+//									|
+//									|
+//	iframe number 4 (new1.4.php)	| iframe number 5 (new1.3.php)
+//									|
+//									|
+//									|
+//------------------------------------------------------------------------				
+//footer(footer.php)
+//WIDTH(5%) HEIGHT(5%)
+//------------------------------------------------------------------------
+?>
